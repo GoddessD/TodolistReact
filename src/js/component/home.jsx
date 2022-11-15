8b4fe00d1a25ce6b20d6af4ea804a2468cef5112
GoddessD <dlouis314@gmail.com>
2022-11-15 23:46:27 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
+import { Input } from "./input.jsx";
+import { List } from "./list.jsx";
+import { Counter } from "./counter.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [list, setList] = useState([])
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Todos</h1>
+			<Input setList = {setList}/>
+			<List list = {list} setList = {setList}  />
+			<Counter numberofItemsLeft = {list.length}/>
 		</div>
 	);
 };
---
+++ src/js/component/list.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Input = () => {
+	return (
+		<div className="text-center">
+			
+		</div>
+	);
+};
+
+
