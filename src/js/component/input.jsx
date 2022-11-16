19be3cb7ee44773bdc414ea2e1d68af11307b91a
GoddessD <dlouis314@gmail.com>
2022-11-16 23:10:27 +0000

    update input

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React,{ useState } from "react";
 
-export const Input = () => {
-	return (
+export const Input = (props) => {
+	const [inputValue,setInputValue] = useState("")
+	const addItem=(e)=> {
+		if (e.key === "Enter" && inputValue!= "" ){
+			let newList = props.list.concat({label:inputValue,done:false})
+			props.setList(newList)
+		}
+	}
+		return (
 		<div className="text-center">
-			
+		<Input onChange={e=>setInputValue(e.target.Value)} onKeyPress={e=>addItem(e)} />	
 		</div>
 	);
 };
